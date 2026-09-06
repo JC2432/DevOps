41bfbb6d443f330d6da3467ea8d1cd0f25b5d0cd
Espericueta <pespericueta132@gmail.com>
2026-09-06 14:48:13 -0600

    agregue schemas para que se puedan crear las inserciones y cambios a la bd

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -2,11 +2,14 @@ from pydantic import BaseModel
 from typing import Optional, List
 from datetime import date
 
-class ClienteBase(BaseModel):
-    nombre: str
-    apellido: str
-    correo: str
-    telefono: Optional[str] = None
+
+class ClienteCreate(BaseModel):
+    NOMBRE: str
+    APELLIDO_P: str
+    APELLIDO_M: Optional[str] = None
+    CORREO: str
+    TELEFONO: Optional[str] = None
+
 
 class LibroCreate(BaseModel):
     EDITORIAL_ID: str
@@ -14,12 +17,15 @@ class LibroCreate(BaseModel):
     ANIO_PUBLICACION: Optional[int] = None
     DISPONIBLE: Optional[bool] = True
 
+
 class LibroUpdate(BaseModel):
     DISPONIBLE: bool
+
 
 class LibroPrestamoItem(BaseModel):
     LIBRO_ID: str
     CANTIDAD: Optional[int] = 1
+
 
 class PrestamoCreate(BaseModel):
     CLIENTE_ID: str
