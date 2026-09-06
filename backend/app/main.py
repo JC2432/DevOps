7167a08c81e73782bb7fd74858328161abfaf553
Espericueta <pespericueta132@gmail.com>
2026-09-06 17:24:34 -0600

    quite el docker

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import prestamos, libros, usuarios, autores, editorial, categorias, clientes
 
 app = FastAPI(
@@ -7,13 +8,21 @@ app = FastAPI(
     version="1.0.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(prestamos.router)
 app.include_router(libros.router)
 app.include_router(usuarios.router)
+app.include_router(clientes.router)
 app.include_router(autores.router)
 app.include_router(editorial.router)
 app.include_router(categorias.router)
-app.include_router(clientes.router)
 
 
 @app.get("/")
